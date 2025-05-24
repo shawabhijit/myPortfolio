2547d1b1f2d0312fcdfc6f40b8874fd3ac9494b0
Abhijit Sahoo <shawabhijit370@gmail.com>
2025-05-24 08:47:43 +0530

    besic setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto p-4'>
+      <h1 className='text-2xl text-white '>My Portfolio</h1>
+    </main>
+  )
+}
+
+export default App
