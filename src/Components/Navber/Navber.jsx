6d63c1016aed65c248dc31d9c36acb52ab07569e
Abhijit Sahoo <shawabhijit370@gmail.com>
2025-05-26 10:53:15 +0530

    changes

--- src/Components/Navber/Navber.jsx
+++ src/Components/Navber/Navber.jsx
@@ -23,7 +23,7 @@ const Navber = () => {
 
   return (
     <div className='text-white fixed top-0 right-0 left-0 bg-black/90'>
-        <div className='max-w-7xl w-full mx-auto'>
+        <div className='max-w-7xl mx-auto'>
             <div className='flex justify-between items-center py-5 mx-auto c-space'>
                 <a href="/" className='text-neutral-400 font-bold text-xl hover:text-white transition-colors'>
                   ABHIJIT
